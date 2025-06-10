740c3856096b64e7399fb542a6a2c8815dc72c15
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-06-10 12:23:02 +0100

    Sketch ex01 solution

---
+++ ex01/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tjorge-l < tjorge-l@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 17:03:59 by tjorge-l          #+#    #+#             */
+/*   Updated: 2025/06/10 12:11:57 by tjorge-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ScavTrap.hpp"
+
+int	main(void)
+{
+	// Constructor and Destructor
+	// ScavTrap	Gorgias("Gorgias");
+
+	// Ouf of Energy
+	// ScavTrap	Gorgias("Gorgias");
+	// int		i = 0;
+	
+	// do 
+	// {
+	// 	Gorgias.attack("Enemy");
+	// 	i++;
+	// }
+	// while (i <= 50);
+
+	// Gate Keeper mode
+	// ScavTrap	Gorgias("Gorgias");
+	// Gorgias.guardGate();
+
+	// Inheritance: BeRepaired
+	// ScavTrap	Gorgias("Gorgias");
+	// Gorgias.beRepaired(3);
+
+	return (0);
+}
